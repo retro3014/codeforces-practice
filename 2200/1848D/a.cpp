44e1571fc97abf305d088edde52e8da899679b21
retro3014 <36194346+retro3014@users.noreply.github.com>
2023-08-13 13:33:21 +0900

    init folders

---
+++ 2200/1848D/a.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define test 1
+#define TEST if(test)
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+ 
+const int MOD = 1000000007; // 998244353
+const int INF = 2e9;
+const ll INFLL = 1e18;
+const int MAX_N = 1000;
+
+int main(){
+
+    return 0;
+}
